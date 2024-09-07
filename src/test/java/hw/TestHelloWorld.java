780ec413ed6706feba13180d8780ab83afa5cf3f
cacayan2 <cacayan2@illinois.edu>
2024-09-07 08:59:49 -0500

    Forgot to add edits to TestHelloWorld.java and HelloWorld.java in last commit.

--- src/test/java/hw/TestHelloWorld.java
+++ src/test/java/hw/TestHelloWorld.java
@@ -21,32 +21,40 @@ public class TestHelloWorld {
     fixture = null;
   }
 
+  // This test verifies that initialization works as expected.
   @Test
   public void initialization() { // this test is OK as-is, it should pass with no problem
     assertNotNull(fixture);
   }
 
+  // Checks that getMessage() method works as expected.
   @Test
   public void getMessage() { // this test is OK as-is, it should pass with no problem
     assertNotNull(fixture);
     assertEquals("hello world", fixture.getMessage());
   }
 
+  // Checks that getYear() method works as expected.
+  // Was corrected because expected year was 2023 instead of 2024.
   @Test
   public void getYear() { // this test is OK, fix HelloWorld.java to make it pass!
     assertNotNull(fixture);
     assertEquals(2024, fixture.getYear());
   }
 
+  // Stores class representation and hashcode of object in list - test uses this list to check getMessage() method by referencing the object through the list.
+  // Corrected as original code attempted to index the list at 1 - Java is 0-indexed so the first element of the list is actually 0.
   @Test
   public void getMessageInList() { // this test is broken - fix it!
     var list = Arrays.asList(fixture);
-    assertEquals("hello world", list.get(1).getMessage());
+    assertEquals("hello world", list.get(0).getMessage());
   }
 
+  // Stores class representation of object in list - test uses this list to check getYear() method by referencing the object through the list.
+  // Corrected as original code attempted to index the list at 1 - Java is 0-indexed so the first element of the list is actually 0.
   @Test
   public void getYearInList() { // this test is broken - fix it!
     var list = Arrays.asList(fixture);
-    assertEquals(2024, list.get(1).getYear());
+    assertEquals(2024, list.get(0).getYear());
   }
 }
